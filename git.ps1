0afab6b863a7115982c1d78a399507b8e14588ad
amittid <122987335+amittid75@users.noreply.github.com>
2024-01-10 10:37:23 +0200

    programcheck.ps1

--- git.ps1
+++ git.ps1
@@ -1,10 +1,10 @@
 Import-Module ActiveDirectory
 
-# Get the current user's OU path (excluding DCs)
+# Get the current user's OU path
 $currentOU = (Get-ADUser -Identity $env:USERNAME).DistinguishedName.Substring($currentUserDN.IndexOf("OU=")).Substring(0, $currentUserDN.IndexOf(",DC="))
 
-# Create a new variable without "OU=Synced", "DC=name",D", and commas
-$modifiedOU = $currentOU -replace "OU=Synced", "" -replace "DC=name",D", "" -replace ",", ""
+# Create a new variable without unwanted DC and OU "OU=name", "DC=name" , and commas
+$modifiedOU = $currentOU -replace "OU=name", "" -replace "DC=name", "" -replace ",", ""
 
 # Get all installed programs (no exclusions)
 $ComputerName = $env:COMPUTERNAME
@@ -14,11 +14,11 @@ $installedPrograms = Get-WmiObject -Class Win32_Product | Select-Object -Propert
 $installedPrograms | Out-File -FilePath ".\progg.txt" -Append
 
 # Construct the destination path with the modified OU
-$destinationPath = "\\127.0.0.1\Archion\prog\$modifiedOU"
+$destinationPath = "\\127.0.0.1\$modifiedOU"
 
 # Send the updated "progg.txt" file to the destination path
 Copy-Item -Path ".\progg.txt" -Destination $destinationPath
 
-# Display the OU path and confirmation message
+# (optional) Display the OU path and confirmation message
 Write-Host "Your current OU path (excluding DCs, synced, specified text, and commas): $modifiedOU"
 Write-Host "File progg.txt (with appended programs) sent to $destinationPath successfully!"
